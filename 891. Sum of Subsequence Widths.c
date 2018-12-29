692732a3c679876d69912d037923d748713408a7
WilliamZhuo <aaa0025235@gmail.com>
2018-12-29 14:29:16 +0800

    Update 891. Sum of Subsequence Widths.c
    Add some ideas

--- 891. Sum of Subsequence Widths.c
+++ 891. Sum of Subsequence Widths.c
@@ -1,3 +1,14 @@
+/*
+2^n time cost 
+1. (improve step 2)can we get the width of one subsequence from two two subsequences(divide and conquer)-> possible
+XX 2. can we record the widths of smaller subsequences(dynamic programming)->difficult->2^n is large
+3. (improve step 1)can we avoid list all subsequences?
+Step1. list all subsequence
+Step2. get the width of each subsequence
+Step3. get the sum of widths
+*/
+
+
 #define NOTEND 0
 #define ISEND 1
 
