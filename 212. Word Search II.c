9cf7905be3c49d13990804fff9f79e3d1e4bd331
WilliamZhuo <aaa0025235@gmail.com>
2019-01-02 21:40:46 +0800

    Update 212. Word Search II.c

--- 212. Word Search II.c
+++ 212. Word Search II.c
@@ -1,4 +1,6 @@
-//DFS -> for each node check wordIindexJ, if the same Ji=J+1
+//Recoding FSM -> 
+// bfs with distance(correct state -> zero distance, wrong state->infinite distance) 
+
 class Solution {
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
